bdcaa8b11dc2a6dac54854c88c001970667a0a76
Dan Cascaval <cascaval@cs.washington.edu>
2022-06-05 16:22:30 -0700

    add formatter

--- rewriter-oracle/synth-numbers.rkt
+++ rewriter-oracle/synth-numbers.rkt
@@ -1,4 +1,4 @@
-#lang rosette/safe
+#lang rosette
 
 (require rosette/lib/destruct)
 
@@ -10,7 +10,8 @@
 (struct div (a b) #:transparent)
 (struct idx (i) #:transparent)
 
-(define ARITY 3) ; Inputs of length 3
+(define INPUT-ARITY 3) ; Inputs of length 3
+(define OUTPUT-ARITY 3) ; Inputs of length 3
 (define DEPTH 3) ; ASTs of depth 3
 
 (define (dz a b) ; Cannot use this because solver will be sad
@@ -33,7 +34,7 @@
 (define (choice-variable)
   (begin
     (define-symbolic* c integer?)
-    (assert (&& (>= c 0) (< c (+ 4 ARITY))
+    (assert (&& (>= c 0) (< c (+ 4 INPUT-ARITY))
                 (not (= c 3)))) ; Outlaw division
     c))
 
@@ -43,7 +44,7 @@
   (bl-rec 0))
 
 (define (input-sequence)
-  (build-list ARITY (lambda (_) (input-variable))))
+  (build-list INPUT-ARITY (lambda (_) (input-variable))))
 
 (define (make-ast heap depth index)
   ; (printf "depth ~a index ~a\n" depth index)
@@ -68,9 +69,9 @@
   (build-list (- (expt 2 depth) 1) (lambda (_) (choice-variable))))
 
 
-(define (make-graph arity depth)
-  (build-list arity (lambda (_)
-                      (make-ast (make-heap depth) depth 0))))
+(define (make-graph graph-heap num-outputs depth)
+  (build-list num-outputs (lambda (i)
+                            (make-ast (list-ref graph-heap i) depth 0))))
 
 (define (lst-equals l1 l2)
   (andmap eq? l1 l2))
@@ -90,35 +91,103 @@
    (not (ormap (lambda (bad)
                  (lst-equals bad (interpret-exprs graph inputs)))
                bad-outputs))
-   (exists-good-input graph inputs good-inputs good-outputs)
-   ))
+   (exists-good-input graph inputs good-inputs good-outputs)))
 
 
 (define (run-synthesis good-outputs bad-outputs)
-  (let
+  (clear-vc!)
+  (let*
       ([inputs (input-sequence)]
        [good-inputs (map (lambda (_) (input-sequence)) good-outputs)]
-       [graph (make-graph ARITY DEPTH)])
-    (synthesize #:forall inputs #:guarantee
-                (assert (synthesis-condition inputs graph good-inputs good-outputs bad-outputs)))))
+       [graph-heap (build-list OUTPUT-ARITY (lambda (_) (make-heap DEPTH)))]
+       [graph (make-graph graph-heap OUTPUT-ARITY DEPTH)]
+       [model (synthesize #:forall inputs #:guarantee
+                          (assert (synthesis-condition inputs graph good-inputs good-outputs bad-outputs))) ]
+       [sln-good-inputs (evaluate good-inputs model)]
+       [sln-program (evaluate graph model)])
+    (printf "Good Inputs = ~a\n" sln-good-inputs)
+    (printf (format-asts sln-program)))
+  (clear-vc!))
 
-; (println (synthesis-condition
-;           (input-sequence)
-;           (make-graph ARITY DEPTH)
-;           (list (input-sequence))
-;           (list '(1 2 3)) (list '(4 5 6))))
+(define (format-asts asts)
+  (define (format-ast ast)
+    (destruct ast
+              [(add a b) (format "(~a + ~a)" (format-ast a) (format-ast b))]
+              [(sub a b) (format "(~a - ~a)" (format-ast a) (format-ast b))]
+              [(mul a b) (format "(~a * ~a)" (format-ast a) (format-ast b))]
+              [(div a b) (format "(~a / ~a)" (format-ast a) (format-ast b))]
+              [(idx a) (format "in[~a]" a)]))
+  (list-ref (foldl (lambda (ast state)
+                     (let
+                         ([index (list-ref state 0)]
+                          [str (list-ref state 1)])
+                       (list (+ index 1) (string-append str (format "out[~a] = " index) (format-ast ast) "\n"))))
+                   (list 0 "") asts) 1))
 
-; (println (synthesis-condition
-;           (input-sequence)
-;           (make-graph ARITY DEPTH)
-;           '() '() (list '(4 5 6))))
-
-; (run-synthesis '() (list '(4 5 6)))
-; (clear-vc!) ; need to clear symbolic state otherwise next query gonna fail
-; (run-synthesis (list '(1 2 3)) '())
-; (clear-vc!)
+; Good Input = [1, 1, 3]
+; (idx 1)                                           // out[0] = in[1]
+; (add (add (idx 2) (idx 1)) (sub (idx 0) (idx 2))) // out[1] = (in[2] + in[1]) + (in[0] - in[2])
+; (sub (mul (idx 2) (idx 1)) (sub (idx 1) (idx 1))) // out[2] = (in[2] * in[1]) - (in[1] - in[1])
 (run-synthesis (list '(1 2 3)) (list '(4 5 6)))
 
+
+
+; (run-synthesis (list '(1 1) '(2 2)) (list '(1 2) '(2 1))) ; scale_param
+; (run-synthesis (list '(1 1) '(1 2)) (list '(2 1) '(2 2))) ; height_param
+; (run-synthesis (list '(1 1) '(2 1)) (list '(1 2) '(2 2))) ; width_param
+
+; (run-synthesis (list ; rotating_box
+;                 '(10 10 13 14 5)
+;                 '(10 10 14 13 5)
+;                 '(10 10 15 10 5)
+;                 '(10 10 13 14 3)
+;                 '(10 10 14 13 6)
+;                 '(10 10 15 10 -1))
+;                (list
+;                 '(8 10 12 14 5)
+;                 '(8 10 12 13 5)
+;                 '(8 10 13 10 5)
+;                 '(10 10 14 14 5)))
+
+; (run-synthesis (list ; rotating_square
+;                 '(10 10 13 14 5)
+;                 '(10 10 14 13 5)
+;                 '(10 10 15 10 5))
+;                (list
+;                 '(10 10 13 14 3)
+;                 '(10 10 14 13 6)
+;                 '(10 10 15 10 -1)
+;                 '(8 10 12 14 5)
+;                 '(8 10 12 13 5)
+;                 '(8 10 13 10 5)
+;                 '(10 10 14 14 5)))
+
+; (run-synthesis (list ; no_rotation
+;                 '(10 10 13 13 3)
+;                 '(10 10 13 13 6)
+;                 '(10 10 15 15 3)
+;                 '(5 6 8 9 3))
+;                (list
+;                 '(10 11 13 13 3)
+;                 '(9 10 13 13 6)
+;                 ))
+
+; (run-synthesis (list ; no_rot_on_axis
+;                 '(10 10 13 13 3)
+;                 '(10 10 13 13 6)
+;                 '(10 10 15 15 3)
+;                 '(5 5 8 8 4))
+;                (list
+;                 '(10 11 13 13 3)
+;                 '(9 10 13 13 6)
+;                 '(5 6 8 9 3)))
+
+
+; (run-synthesis (list '(2 1 5) '(2 1 10)) (list '(4 2 5))) ; more_stairs
+; (run-synthesis (list '(2 1 5) '(4 2 5))  (list '(2 1 10))) ; big_stairs
+
+; (run-synthesis (list '(10 5 5) '(20 10 10)) (list '(20 10 5))) ; endpoint_more_stairs
+; (run-synthesis (list '(10 5 5) '(20 10 5)) (list '(20 10 10))) ; endpoint_big_stairs
 
 
 ; (println (add (sub (idx 1) (idx 3)) (add (idx 2) (idx 2))))
