2eae9f7feb0239d5bcf9711722397aae4509e039
Dan Cascaval <cascaval@cs.washington.edu>
2022-06-05 12:13:54 -0700

    switch to heap enumeration

--- rewriter-oracle/synth-numbers.rkt
+++ rewriter-oracle/synth-numbers.rkt
@@ -11,6 +11,7 @@
 (struct idx (i) #:transparent)
 
 (define ARITY 3) ; Inputs of length 3
+(define DEPTH 3) ; ASTs of depth 3
 
 (define (interpret input expr)
   (destruct expr
@@ -32,12 +33,6 @@
     (assert (&& (>= 0 c) (< (+ 4 ARITY) c)))
     c))
 
-(define (root-choice-var)
-  (begin
-    (define-symbolic* r integer?)
-    (assert (&& (>= 0 r) (< ARITY r)))
-    r))
-
 (define (input-sequence)
   (build-list ARITY (lambda (k) (input-variable))))
 
@@ -51,27 +46,36 @@
               (lst-equals (interpret-exprs graph good-input) good-output)))
    good-outputs))
 
-(define (make-ast depth)
-  (if (= 0 depth)
-      (idx (root-choice-var))
-      (let
-          ([choice-var (choice-variable)]
-           [next-depth (- depth 1)])
-        (cond
-          [(= choice-var 0) (add (make-ast next-depth) (make-ast next-depth))]
-          [(= choice-var 1) (sub (make-ast next-depth) (make-ast next-depth))]
-          [(= choice-var 2) (mul (make-ast next-depth) (make-ast next-depth))]
-          [(= choice-var 3) (div (make-ast next-depth) (make-ast next-depth))]
-          [else (idx (- choice-var 4))])
-        )))
+(define (make-ast heap depth index)
+  (let
+      ([choice-var (list-ref heap index) ])
+    (if (= 0 depth)
+        (begin
+          (assert (>= 4 choice-var))
+          (idx (- choice-var 4)))
+        (let
+            ([next-depth (- depth 1)]
+             [left (+ (* 2 index) 1)]
+             [right (+ (* 2 index) 2)])
+          (cond
+            [(= choice-var 0) (add (make-ast heap next-depth left) (make-ast heap next-depth right))]
+            [(= choice-var 1) (sub (make-ast heap next-depth left) (make-ast heap next-depth right))]
+            [(= choice-var 2) (mul (make-ast heap next-depth left) (make-ast heap next-depth right))]
+            [(= choice-var 3) (div (make-ast heap next-depth left) (make-ast heap next-depth right))]
+            [else (idx (- choice-var 4))])
+          ))))
 
-(define (make-graph num)
-  (build-list ARITY (lambda (x) (make-ast ARITY))))
+(define (make-heap depth)
+  (build-list (expt 2 depth) (lambda (_) (choice-variable))))
+
+
+(define (make-graph arity depth)
+  (build-list arity (lambda (_)
+                      (make-ast (make-heap depth) depth 1))))
 
 (define (synthesis-condition inputs graph good-inputs good-outputs bad-outputs)
   (and
    (not (ormap (lambda (bad)
-                 (println bad)
                  (lst-equals bad (interpret-exprs graph inputs)))
                bad-outputs))
    (exists-good-input graph inputs good-inputs good-outputs)))
@@ -81,16 +85,16 @@
   (let
       ([inputs (input-sequence)]
        [good-inputs (map (lambda (g) (input-sequence)) good-outputs)]
-       [graph (make-graph ARITY)])
+       [graph (make-graph ARITY DEPTH)])
     (synthesize #:forall inputs #:guarantee
                 (synthesis-condition inputs graph good-inputs good-outputs bad-outputs))))
 
 
-; (print (make-ast 3))
-; (print (synthesis-condition
-;         '(1 2 3)
-;         (make-graph ARITY)
-;         '(9 8 7)
-;         (list '(1 2 3)) (list '(4 5 6))))
+; (print (make-graph 3 3))
+(print (synthesis-condition
+        '(1 2 3)
+        (make-graph ARITY DEPTH)
+        '(9 8 7)
+        (list '(1 2 3)) (list '(4 5 6))))
 
-(run-synthesis 3 (list '(1 2 3)) (list '(4 5 6)))
+; (run-synthesis 3 (list '(1 2 3)) (list '(4 5 6)))
