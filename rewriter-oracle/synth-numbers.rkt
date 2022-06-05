1aa4e84426b8e26e0a26c64c83107793349cf2a7
Dan Cascaval <cascaval@cs.washington.edu>
2022-06-05 14:25:39 -0700

    remove division operator

--- rewriter-oracle/synth-numbers.rkt
+++ rewriter-oracle/synth-numbers.rkt
@@ -13,12 +13,14 @@
 (define ARITY 3) ; Inputs of length 3
 (define DEPTH 3) ; ASTs of depth 3
 
+(define (nonzero element) element)
+
 (define (interpret input expr)
   (destruct expr
             [ (add a b) (+ (interpret input a) (interpret input b)) ] ; 0
             [ (sub a b) (- (interpret input a) (interpret input b)) ] ; 1
-            [ (mul a b) (/ (interpret input a) (interpret input b)) ] ; 2
-            [ (div a b) (* (interpret input a) (interpret input b)) ] ; 3
+            [ (mul a b) (* (interpret input a) (interpret input b)) ] ; 2
+            [ (div a b) (/ (interpret input a) (nonzero (interpret input b))) ] ; 3
             [ (idx i) (list-ref input i) ]))                          ; 4 -> (4+arity-1)
 
 (define (interpret-exprs exprs input)
@@ -30,7 +32,7 @@
 (define (choice-variable)
   (begin
     (define-symbolic* c integer?)
-    (assert (&& (>= c 0) (< c (+ 4 ARITY))))
+    (assert (&& (>= c 0) (< c (+ 4 ARITY)) (not (eq? c 3))))
     c))
 
 (define (input-sequence)
@@ -76,33 +78,69 @@
    good-outputs))
 
 (define (synthesis-condition inputs graph good-inputs good-outputs bad-outputs)
-  (printf "~a ~a ~a ~a\n" inputs good-inputs good-outputs bad-outputs)
+  ; (printf "~a ~a ~a ~a\n" inputs good-inputs good-outputs bad-outputs)
   (and
    (not (ormap (lambda (bad)
                  (lst-equals bad (interpret-exprs graph inputs)))
                bad-outputs))
-   (exists-good-input graph inputs good-inputs good-outputs)))
+   (exists-good-input graph inputs good-inputs good-outputs)
+   ))
 
 
 (define (run-synthesis good-outputs bad-outputs)
   (let
       ([inputs (input-sequence)]
        [good-inputs (map (lambda (_) (input-sequence)) good-outputs)]
-       [graph (make-graph ARITY DEPTH)]
-       )
+       [graph (make-graph ARITY DEPTH)])
     (synthesize #:forall inputs #:guarantee
                 (assert (synthesis-condition inputs graph good-inputs good-outputs bad-outputs)))))
-
-(println (synthesis-condition
-          (input-sequence)
-          (make-graph ARITY DEPTH)
-          (list (input-sequence))
-          (list '(1 2 3)) (list '(4 5 6))))
 
 ; (println (synthesis-condition
 ;           (input-sequence)
 ;           (make-graph ARITY DEPTH)
-;           '() '() '()))
+;           (list (input-sequence))
+;           (list '(1 2 3)) (list '(4 5 6))))
 
+; (println (synthesis-condition
+;           (input-sequence)
+;           (make-graph ARITY DEPTH)
+;           '() '() (list '(4 5 6))))
 
+; (run-synthesis '() (list '(4 5 6)))
+; (clear-vc!) ; need to clear symbolic state otherwise next query gonna fail
+; (run-synthesis (list '(1 2 3)) '())
+; (clear-vc!)
 (run-synthesis (list '(1 2 3)) (list '(4 5 6)))
+
+
+
+; (println (add (sub (idx 1) (idx 3)) (add (idx 2) (idx 2))))
+
+(define (synthesis-example)
+  ; This is the general outline of what we're trying to do. For a given input variable (i0)
+  ; Find a model for c0 that defines a function that meets an output criterion.
+  (define inputs (list (input-variable)))
+  (define good-inputs (list (input-variable)))
+  (define choice (list (choice-variable)))
+
+  (define (execute input)
+    (let
+        ([c0 (list-ref choice 0)]
+         [a (list-ref input 0)]   ; Drawing from the same input argument.
+         [b (list-ref input 0)])  ;
+      (cond
+        [(= c0 0) (+ a b)]
+        [(= c0 1) (- a b)]
+        [(= c0 2) (* a b)]
+        [(= c0 3) (begin (assume (not (eq? b 0))) (/ a b)) ] )))
+
+  (synthesize #:forall inputs #:guarantee
+              (assert (and
+                       (not (eq? (execute inputs) 3))   ; will NEVER be 3
+                       (implies (lst-equals inputs good-inputs)
+                                (= (execute inputs) 4)) ; will FOR SOME input be 4
+                       )))
+  )
+;(synthesis-example)
+
+
