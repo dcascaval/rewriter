81911f89546dd8566a9b25d25a0a71b0e3c58713
Dan Cascaval <cascaval@cs.washington.edu>
2022-06-05 17:11:07 -0700

    now i have completely broken it, and i do not know why

--- rewriter-oracle/synth-numbers.rkt
+++ rewriter-oracle/synth-numbers.rkt
@@ -11,19 +11,15 @@
 (struct idx (i) #:transparent)
 
 (define INPUT-ARITY 3) ; Inputs of length 3
-(define OUTPUT-ARITY 3) ; Inputs of length 3
-(define DEPTH 3) ; ASTs of depth 3
-
-(define (dz a b) ; Cannot use this because solver will be sad
-  (if (= b 0) 0 (/ a b)))
+(define DEPTH 2) ; ASTs of depth 3
+(define NUM-OPERATORS 3)
 
 (define (interpret input expr)
   (destruct expr
             [ (add a b) (+ (interpret input a) (interpret input b)) ] ; 0
             [ (sub a b) (- (interpret input a) (interpret input b)) ] ; 1
             [ (mul a b) (* (interpret input a) (interpret input b)) ] ; 2
-            [ (div a b) (/ (interpret input a) (interpret input b)) ] ; 3
-            [ (idx i) (list-ref input i) ]))                          ; 4 -> (4+arity-1)
+            [ (idx i) (list-ref input i) ]))                          ; 3 -> (3+arity-1)
 
 (define (interpret-exprs exprs input)
   (map (curry interpret input) exprs))
@@ -34,8 +30,7 @@
 (define (choice-variable)
   (begin
     (define-symbolic* c integer?)
-    (assert (&& (>= c 0) (< c (+ 4 INPUT-ARITY))
-                (not (= c 3)))) ; Outlaw division
+    (assert (&& (>= c 0) (< c (+ 3 INPUT-ARITY))))
     c))
 
 (define (build-list n f)
@@ -52,8 +47,8 @@
       ([choice-var (list-ref heap index) ])
     (if (= 1 depth)
         (begin
-          (assert (>= choice-var 4))
-          (idx (- choice-var 4)))
+          (assert (>= choice-var NUM-OPERATORS))
+          (idx (- choice-var NUM-OPERATORS)))
         (let
             ([left (make-ast heap (- depth 1) (+ (* 2 index) 1)) ]
              [right (make-ast heap (- depth 1) (+ (* 2 index) 2)) ])
@@ -61,8 +56,7 @@
             [(= choice-var 0) (add left right)]
             [(= choice-var 1) (sub left right)]
             [(= choice-var 2) (mul left right)]
-            [(= choice-var 3) (div left right)]
-            [else (idx (- choice-var 4))])
+            [else (idx (- choice-var NUM-OPERATORS))])
           ))))
 
 (define (make-heap depth)
@@ -93,14 +87,21 @@
                bad-outputs))
    (exists-good-input graph inputs good-inputs good-outputs)))
 
+; TODO: Check all outputs are the same length
+(define (get-arity outputs)
+  (if (= (length outputs) 0) null
+      (length (list-ref outputs 0))))
 
 (define (run-synthesis good-outputs bad-outputs)
   (clear-vc!)
   (let*
       ([inputs (input-sequence)]
        [good-inputs (map (lambda (_) (input-sequence)) good-outputs)]
-       [graph-heap (build-list OUTPUT-ARITY (lambda (_) (make-heap DEPTH)))]
-       [graph (make-graph graph-heap OUTPUT-ARITY DEPTH)]
+       [out-arity (get-arity (append good-outputs bad-outputs))]
+       [graph-heap (build-list out-arity (lambda (_) (make-heap DEPTH)))]
+       [graph (make-graph graph-heap out-arity DEPTH)]
+       [_ (println (list inputs good-inputs out-arity graph-heap graph))]
+       [_ (println (synthesis-condition inputs graph good-inputs good-outputs bad-outputs))]
        [model (synthesize #:forall inputs #:guarantee
                           (assert (synthesis-condition inputs graph good-inputs good-outputs bad-outputs))) ]
        [sln-good-inputs (evaluate good-inputs model)]
@@ -125,12 +126,12 @@
                    (list 0 "") asts) 1))
 
 ; Good Input = [1, 1, 3]
-; (idx 1)                                           // out[0] = in[1]
-; (add (add (idx 2) (idx 1)) (sub (idx 0) (idx 2))) // out[1] = (in[2] + in[1]) + (in[0] - in[2])
-; (sub (mul (idx 2) (idx 1)) (sub (idx 1) (idx 1))) // out[2] = (in[2] * in[1]) - (in[1] - in[1])
+;   out[0] = in[1]
+;   out[1] = (in[2] + in[1]) + (in[0] - in[2])
+;   out[2] = (in[2] * in[1]) - (in[1] - in[1])
 (run-synthesis (list '(1 2 3)) (list '(4 5 6)))
 
-
+; (run-synthesis (list '(1 1)) (list '(1 2) '(2 1))) ; scale_param
 
 ; (run-synthesis (list '(1 1) '(2 2)) (list '(1 2) '(2 1))) ; scale_param
 ; (run-synthesis (list '(1 1) '(1 2)) (list '(2 1) '(2 2))) ; height_param
@@ -190,13 +191,16 @@
 ; (run-synthesis (list '(10 5 5) '(20 10 5)) (list '(20 10 10))) ; endpoint_big_stairs
 
 
-; (println (add (sub (idx 1) (idx 3)) (add (idx 2) (idx 2))))
+
+
 
 (define (synthesis-example)
+  (clear-vc!)
   ; This is the general outline of what we're trying to do. For a given input variable (i0)
   ; Find a model for c0 that defines a function that meets an output criterion.
   (define inputs (list (input-variable)))
-  (define good-inputs (list (input-variable)))
+  (define good-1 (list (input-variable)))
+  (define good-2 (list (input-variable)))
   (define choice (list (choice-variable)))
 
   (define (execute input)
@@ -207,16 +211,21 @@
       (cond
         [(= c0 0) (+ a b)]
         [(= c0 1) (- a b)]
-        [(= c0 2) (* a b)]
-        [(= c0 3) (begin (assume (not (eq? b 0))) (/ a b)) ] )))
+        [(= c0 2) (* a b)])))
 
   (synthesize #:forall inputs #:guarantee
               (assert (and
-                       (not (eq? (execute inputs) 3))   ; will NEVER be 3
-                       (implies (lst-equals inputs good-inputs)
-                                (= (execute inputs) 4)) ; will FOR SOME input be 4
+                       (not (eq? (execute inputs) 3)) ;; Unconditionally never 3
+                       (and
+                        (implies
+                         (lst-equals inputs good-1)
+                         (= (execute inputs) 4)) ;; For some input, it's 4 (input = 2)
+                        (implies
+                         (lst-equals inputs good-2)
+                         (= (execute inputs) 2))) ;; For some other one, it's 2 (input = 1)
                        )))
-  )
-;(synthesis-example)
+  (clear-vc!))
+
+; (synthesis-example)
 
 
